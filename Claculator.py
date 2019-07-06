e1e0e758c5a4ec59e4ea751b1229664bbb5ebaab
abdallah-eldali <52472969+abdallah-eldali@users.noreply.github.com>
2019-07-06 17:50:21 -0400

    Created a for-loop to simplify the instantiation of the buttons

--- Claculator.py
+++ Claculator.py
@@ -3,6 +3,8 @@ from math import *
 
 pointer = 0
 
+#-----------------------------------------------------------------------
+
 class createButton:
     def __init__(self, master, text, location, entry):
         self.button = Button(master, text=text, command=self.onClick)
@@ -14,8 +16,11 @@ class createButton:
 
     def onClick(self):
         global pointer
-        if self.text == "Enter" or self.text == "Clear":
+        if self.text == "Enter":
             pass
+        elif self.text == "Clear":
+            self.entry.delete(0, pointer)
+            pointer = 0
         else:
             if self.text == "./":
                 self.entry.insert(pointer, "./(")
@@ -24,29 +29,41 @@ class createButton:
                 self.entry.insert(pointer, self.text)
                 pointer += len(self.text)
 
+#-----------------------------------------------------------------------
 
 def createEntry(master, row=0, column=0, columnspan=1):
     entry = Entry(master)
     entry.grid(row=row, column=column, columnspan=columnspan)
     return entry
 
+def calculate(entry):
+    inputCalculation = entry.get()
+    if "./" in inputCalculation:
+        calculation = inputCalculation.replace("./", "sqrt")
+    if "^" in inputCalculation:
+        calculation = inputCalculation.replace("^", "**")
+    
+    return str(eval(calculation))
+
 def main():
 
     tk = Tk()
 
-    entry = createEntry(tk, columnspan=5)
+    entry = createEntry(tk, columnspan=3)
 
     #Numbers:
-    button7 = createButton(tk, "7", (1, 0), entry)
-    button8 = createButton(tk, "8", (1, 1), entry)
-    button9 = createButton(tk, "9", (1, 2), entry)
-    button4 = createButton(tk, "4", (2, 0), entry)
-    button5 = createButton(tk, "5", (2, 1), entry)
-    button6 = createButton(tk, "6", (2, 2), entry)
-    button1 = createButton(tk, "1", (3, 0), entry)
-    button2 = createButton(tk, "2", (3, 1), entry)
-    button3 = createButton(tk, "3", (3, 2), entry)
     button0 = createButton(tk, "0", (4, 1), entry)
+
+    numberList = ["7", "8", "9", "4", "5", "6", "1", "2", "3"]
+    pointer = 0
+
+    for rows in range(1, 4):
+        for columns in range(3):
+            print(rows, columns)
+            print(numberList[pointer])
+            button = createButton(tk, numberList[pointer], (rows, columns), entry)
+            pointer += 1
+
 
     #Signs
     buttonPlus = createButton(tk, "+", (1, 3), entry)
@@ -55,6 +72,19 @@ def main():
     buttonDivid = createButton(tk, "/", (2, 4), entry)
     buttonRoot = createButton(tk, "./", (3, 3), entry)
     buttonSqr = createButton(tk, "^(2)", (3, 4), entry)
+
+    #Enter & Clear
+    butttonEnter = createButton(tk, "Enter", (4, 0), entry)
+    buttonClear = createButton(tk, "Clear", (4, 2), entry)
+
+    #Checking
+    res = Label(tk)
+    res.grid(row=0, column=3, columnspan=2)
+
+    res.configure(text="= ")
+
+
+
 
     tk.mainloop()
 
